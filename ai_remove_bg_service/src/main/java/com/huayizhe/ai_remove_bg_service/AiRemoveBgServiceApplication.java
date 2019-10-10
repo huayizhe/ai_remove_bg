935b60512d368a5e93caa74d37ec238e1edc3896
'huayizhe' <1035864725@qq.com>
2019-10-10 16:33:49 +0800

    初次提交空白项目

---
+++ ai_remove_bg_service/src/main/java/com/huayizhe/ai_remove_bg_service/AiRemoveBgServiceApplication.java
@@ -0,0 +1,13 @@
+package com.huayizhe.ai_remove_bg_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiRemoveBgServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiRemoveBgServiceApplication.class, args);
+	}
+
+}
---
+++ ai_remove_bg_service/src/test/java/com/huayizhe/ai_remove_bg_service/AiRemoveBgServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.huayizhe.ai_remove_bg_service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AiRemoveBgServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
